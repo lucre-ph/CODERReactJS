fbaf0d3c0be57fb913b0897e659ea60c674acd8b
lucre-ph <lucre.ph@hotmail.com>
2021-06-08 19:16:42 -0300

    styling

--- src/Components/itemListContainer/ItemListContainer.js
+++ src/Components/itemListContainer/ItemListContainer.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
+const style = {textAlign: 'center', color: 'pink', fontWeight: '800'}
 export const ItemListContainer = () => {
   const greeting = '¡Hola! Acá va a ir el catálogo';
     return <>
-      <p>{greeting}</p>
+      <p style={style}>{greeting}</p>
     </>
   };
