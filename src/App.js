fbd0057d76246a7e21c51807a2445179b16abd63
lucre-ph <lucre.ph@hotmail.com>
2021-06-08 21:29:47 -0300

    modificaciones

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
 import ButtonAppBar from './components/navbar/Navbar';
-import {CartWidget} from './components/cartWidget/CartWidget';
 import {ItemListContainer} from './components/itemListContainer/ItemListContainer';
 
 const App = () => {
   return <>
-    <ButtonAppBar>    
-      <CartWidget/>
-    </ButtonAppBar>
-    <ItemListContainer/>
+    <ButtonAppBar/>    
+    <ItemListContainer greeting='¡Hola! Acá va a ir el catálogo'/>
   </>
 }
 
