60a6998387d7b4836b2805bbda16971cd3ff9ccd
lucre-ph <lucre.ph@hotmail.com>
2021-06-17 23:54:27 -0300

    Detail

---
+++ src/screens/ItemDetail/ItemDetail.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+/*Debe mostrar la vista de detalle de un item incluyendo descripcion, foto y precio*/
---
+++ src/screens/ItemDetailContainer/ItemDetailContainer.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+/*Al iniciar utilizando un efecto de montaje debe llamar a una promise que en 2segs devuelva
+un item y lo guarde en un estado*/
