e3638eea3dc140f0b5dd093202e717604de7da50
lucre-ph <lucre.ph@hotmail.com>
2021-06-21 16:14:10 -0300

    entrega6

--- src/screens/itemList/ItemList.js
+++ src/screens/itemList/ItemList.js
@@ -5,7 +5,9 @@ import {Item} from '../item/Item';
 //COMPONENTE ITEMLIST
 export const ItemList = arrayProductos => {
     
-    return arrayProductos.map((producto, i) => 
-        <Item producto={producto} i={i}/>)
+    return <React.Fragment>
+        {arrayProductos.map((producto, i) => {
+            <Item producto={producto} i={i}/> })}
+    </React.Fragment>
     
 }
