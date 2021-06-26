28db1941075db9384d95ff09eab8b8e294b00f69
lucre-ph <lucre.ph@hotmail.com>
2021-06-26 02:16:18 -0300

    eventos

--- src/screens/ItemDetailContainer/components/ItemCount/ItemCount.js
+++ src/screens/ItemDetailContainer/components/ItemCount/ItemCount.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+
 import './ItemCountStyle.css'
 import 'materialize-css/dist/css/materialize.min.css'
 import {Link} from 'react-router-dom';
@@ -9,14 +10,8 @@ import {Link} from 'react-router-dom';
 const ItemCount = props => {
     return <h3>{`${props.stock}`}</h3> 
 }
-//     function removeItem() {
-//         setCount(conteoItems>initial && conteoItems-1)};
-//     function addItem() {
-//         setCount(conteoItems<stock && conteoItems+1)};
-//     return <div className="itemCount">     
-//         {conteoItems>=stock && <h5>¡Lo lamentamos! No hay más stock de este producto <SentimentVeryDissatisfiedIcon/></h5>}</div>}
 
-//FALTA INHABILITAR LINK HASTA QUE EL STOCK SEA DISTINTO DE 0
+//FALTA INHABILITAR LINK HASTA QUE EL STOCK SEA DISTINTO DE 0(initial)
 const ButtonsComponent = ({cambiarStock, stock}) => {
     return <>
        <div>
@@ -24,11 +19,11 @@ const ButtonsComponent = ({cambiarStock, stock}) => {
           <button className="waves-effect waves-light btn-small" onClick={() => cambiarStock(stock-1)}>-</button>
        </div>
        <span>
-          <button disabled={stock>=0}><Link to='/Cart'>{`Add to cart`}<AddShoppingCartIcon/></Link></button>
+          {/* <button className="agregar" onClick={() => onAdd(conteoItems)}>Add to cart <AddShoppingCartIcon/></button> */}
+          <button disabled={true}><Link to='/Cart' onClick={e => e.preventDefault()} style={{ pointerEvents: 'none' }}>{`Finalizar compra`}<AddShoppingCartIcon/></Link></button>
        </span>
     </>
 }
-
 
 const ItemCountContainer = ({componente: StockHandler}) => {
     const initial = 0;
@@ -42,3 +37,23 @@ const ItemCountContainer = ({componente: StockHandler}) => {
 export const EventoCompra = props => {
     return <ItemCountContainer componente={ButtonsComponent}/>
 }
+
+
+
+//ITEMCOUNT PRIMER ENTRETA -VIEJO
+// export function ItemCount ({stock, initial, onAdd}) {
+//     const [conteoItems, setCount] = useState(initial);
+//     function removeItem() {
+//         setCount(conteoItems>initial && conteoItems-1)
+//     };
+//     function addItem() {
+//         setCount(conteoItems<stock && conteoItems+1)
+//     };
+//     return <div className="itemCount">
+//         <button disabled={conteoItems===initial} onClick= {removeItem}> - </button>        
+//         <h2>{conteoItems}</h2>
+//         <button disabled={conteoItems===stock} onClick= {addItem}> + </button>    
+//         {conteoItems>=stock && <h2>¡Lo lamentamos! No hay más stock de este producto <SentimentVeryDissatisfiedIcon/></h2>}
+//         <span><button className="agregar" onClick={() => onAdd(conteoItems)}>Add to cart <AddShoppingCartIcon/></button></span>
+//     </div>
+// }
