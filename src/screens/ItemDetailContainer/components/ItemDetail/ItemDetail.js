d8d4d11042cddf7ee797b733f3590313f66fbca5
lucre-ph <lucre.ph@hotmail.com>
2021-07-01 01:35:02 -0300

    eventos

--- src/screens/ItemDetailContainer/components/ItemDetail/ItemDetail.js
+++ src/screens/ItemDetailContainer/components/ItemDetail/ItemDetail.js
@@ -8,18 +8,20 @@ import {ItemCount} from '../ItemCount/ItemCount';
 export const ItemDetail = ({...item}) => {    
     const [finalizarCompra, setFinalizarCompra] = useState (false)
 
-    function onAdd () {
-            ({conteoItems} > 0 && setFinalizarCompra (true))
+    function onAdd (conteoItems) {   
+        setFinalizarCompra (true)
+        console.log(conteoItems)
     }
 
     return <>
         <Divider/>
+        
         <Card variant="outlined">
             <img alt={item.alt} src={item.img}/>
             <h3>{item.title}</h3>
             <h4>{item.detail}</h4>
             <h5>${item.price}</h5> 
-            {finalizarCompra(true) ? <button><Link to='/Cart'>{`Finalizar compra`}</Link></button> : <ItemCount initial={1} stock={item.stock} onAdd={onAdd}/>}
+            {finalizarCompra ? <><button><Link to='/Cart'>{`Finalizar compra`}</Link></button><h6>{`Agregaste unidades de este producto al carrito`}</h6></> : <ItemCount initial={1} stock={item.stock} onAdd={onAdd}/>}
             <Link to='/'><button>Volver atrás</button></Link>
         </Card>
     </>
