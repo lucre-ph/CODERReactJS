9e52533035a3fe557ad01de3aaff40e46ece0b46
lucre-ph <lucre.ph@hotmail.com>
2021-07-10 03:48:25 -0300

    context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 import {Ruteo} from './Router/Ruteo';
 import {CartContextProvider} from './Context/CartContext';
--- src/screens/itemListContainer/components/item/Item.js
+++ src/screens/itemListContainer/components/item/Item.js
@@ -7,8 +7,8 @@ export const Item = ({...producto}) => {
    
    return <Card className="col s4 card darken-1">
       <Link to={`/item/${producto.id}`}><img alt={producto.alt} src={producto.img}/></Link>
-      <h3>{producto.title}</h3>
-      <h5>${producto.price}</h5>
+      <h3 className="flow-text">{producto.title}</h3>
+      <h8 className="flow-text">${producto.price}</h8>
       <Link to={`/item/${producto.id}`}><button className="waves-effect waves-light btn-small">Agregar al carrito</button></Link>
    </Card>
 }
