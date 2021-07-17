2e2c5702b14d1c207128381e8f307d53ddee6e61
lucre-ph <lucre.ph@hotmail.com>
2021-07-17 19:54:44 -0300

    cartView

--- src/screens/CartContainer/components/Cart.js
+++ src/screens/CartContainer/components/Cart.js
@@ -17,18 +17,18 @@ export const Cart = () => {
     const {clearCart, agregadosAlCarrito} = useContext(CartContext);
 
     return <section>
-        {agregadosAlCarrito.map((producto, i) => {
-            return <table className="responsive-table highlight">
-                <thead>
-                    <tr>
-                        <th>{` `}</th>
-                        <th>{`Producto`}</th>
-                        <th>{`Cantidad`}</th>
-                        <th>{`Precio`}</th>
-                        <th>{` `}</th>
-                    </tr>
-                </thead>
-                <tbody>
+        <table className="responsive-table highlight">
+            <thead>
+                <tr>
+                    <th>{` `}</th>
+                    <th>{`Producto`}</th>
+                    <th>{`Cantidad`}</th>
+                    <th>{`Precio`}</th>
+                    <th>{` `}</th>
+                </tr>
+            </thead>
+            <tbody>
+            {agregadosAlCarrito.map((producto, i) => (
                     <tr>
                         <td><img className={classes.img} alt={`${producto.item.alt}`} src={`${producto.item.img}`}/></td>
                         <td>{`${producto.item.title}`}</td>
@@ -36,9 +36,9 @@ export const Cart = () => {
                         <td>{`${producto.item.price}`}</td>
                         <td><button className="waves-effect waves-light btn"><i className="material-icons left"></i>{`Eliminar producto`}</button></td>
                     </tr>
-                </tbody>
-            </table> 
-        })}
+            ))}
+            </tbody>
+        </table> 
         {/* <button onClick={removeItem()}></button> */}
         <button onClick={() => clearCart()}>Vaciar carrito</button>
         <button onClick={e=>purchase()}>{`Finalizar compra`}</button>
