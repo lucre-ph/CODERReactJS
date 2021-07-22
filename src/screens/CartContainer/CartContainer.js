4cc50603dc86850cf330c37f345c40fcedef149c
lucre-ph <lucre.ph@hotmail.com>
2021-07-22 19:28:42 -0300

    firebaseII

--- src/screens/CartContainer/CartContainer.js
+++ src/screens/CartContainer/CartContainer.js
@@ -2,9 +2,9 @@ import React, {useContext} from 'react';
 import {Link} from 'react-router-dom';
 import {CartContext} from '../../Context/CartContext';
 import {Cart} from './components/Cart';
-// import firebase from 'firebase/app';
-// import 'firebase/firestore';
-// import {dataBase} from '../../Firebase/firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import {dataBase} from '../../Firebase/firebase';
 
 const CarritoVacio = () => {
     return <div>
@@ -13,81 +13,48 @@ const CarritoVacio = () => {
     </div>
 }
 
-// const generarOrden = () => {}
+const GenerarOrdenActualizarStock = () => {
+    const {agregadosAlCarrito, precio} = useContext(CartContext);   
+    const updateItems = dataBase.collection("items")//.doc("//ID que saco de firebase//");
+    .where(firebase.firestore.FieldPath.documentId(), 'in', agregadosAlCarrito.map(i => i.item.id));            
+        
+    updateItems.get().then(data => {
+        const batch = dataBase.batch();
+        const outOfStock = [];
 
-// const actualizarStock = () => {}
+        data.docs.forEach((documento, id) => {
+        if (documento.data().stock >= "items"[id].quantity) {
+            batch.update(updateItems, {"stock": documento.data().stock - "items"[id].quantity});
+        } else {
+            outOfStock.push({ ...documento.data(), id: documento.id});
+        }
+        })
 
-                    //     const itemsToUpdate = dataBase.collection("items")
-                    //       .where(firebase.firestore.FieldPath.documentId(), 'in', items.map(i => i.item.id));
-                    
-                    //     itemsToUpdate.get().then(querySnapshot => {
-// const batch = dataBase.batch();
-// const outOfStock = [];
-                    
-                    //       querySnapshot.docs.forEach((docSnapshot, idx) => {
-                    //         if (docSnapshot.data().stock >= items[idx].quantity) {
-                    //           batch.update(docSnapshot.ref, { stock: docSnapshot.data().stock - items[idx].quantity });
-                    //         } else {
-                    //           outOfStock.push({ ...docSnapshot.data(), id: docSnapshot.id });
-                    //         }
-                    //       })
-                    
-// if (outOfStock.length === 0) {
-//     const newOrder = dataBase.collection("orders").doc();  //CREA UN DOCUMENTO DENTRO DE LA COLECCION ITEM Y AUTOGENERA EL ID con .doc()
-//     batch.set(newOrder, {
-//         date: firebase.firestore.Timestamp.fromDate(new Date())
-//         buyer: userInfo,
-//         items: agregadosAlCarrito, //title, description, price
-//         total: precio
-// };
-                    //         })
-                    //         batch.commit().then(() => {
-                    //           dataBase.collection("orders") 
-                    //             .orderBy('date', "desc").limit(1)
-                    //             .get().then(querySnapshot => console.log(querySnapshot.docs[0].id))
-                    //         });
-                    //       }
-                    //     }
-                    //     )
-/*
-const updateItem =dataBase.collection("items").doc("fsrs546+455");
-batch.update(updateItem, {"price": '100'});
-
-batch.commit().then(dat =>
-    console.log(dat, '¿que valor tiene dat?)
-});    
-*/
-
-/*ACTUALIZAR STOCK DE COMPRADOS
-const item = dataBase.collection("items").doc(id);
-
-return item.update({
-    stock: 
-})
-    .then(()=> {
-        console.log("stock actualizado");
+    if (outOfStock.length === 0) {
+        const newOrder = dataBase.collection("orders").doc();  
+        batch.set(newOrder, {
+            // buyer: userInfo,{name, phone, mail}
+            items: [agregadosAlCarrito.id, agregadosAlCarrito.title, agregadosAlCarrito.price], //title, description, price
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+            total: precio
+        });
+    
+    batch.commit().then(() => {
+        dataBase.collection("orders") 
+        .orderBy('date', "desc").limit(1)
+        .get().then(data => console.log(data.docs[0].id))
+        console.log("stock actualizado")
+            
+        return <h2>{`Número de orden: ${newOrder.id}.¡Muchas gracias por tu compra!`}</h2>
+        });
+    }
     })
-    .catch
-    .finally   ...........*/
-
-// const orders = dataBase.collection('orders');
-// const newOrder = {
-//     buyer: userInfo,
-//     items: cart,
-//     total: price()
-// };
-
-////creacion----> orders.add(newOrder).then(({id}) => {
-            //     setOrderId(id)}) //SUCCESS
-            // }).catch(err => {
-            //     setError(err); //ERROR
-            // }).finally(() => {
-            //     setLoading(false);
-            // });
+}
 
 export const CartContainer = () => {
     const {agregadosAlCarrito} = useContext(CartContext);    
-     
+    GenerarOrdenActualizarStock()
+
     return <section>
         {agregadosAlCarrito.length > 0 ? <Cart/> : <CarritoVacio/>}
     </section>
